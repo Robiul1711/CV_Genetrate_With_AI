a54e0e3066c308138fffbc04ca790394d315c54f
Robiul1711 <ashiq.web.dev@gmail.com>
2025-08-21 11:41:06 +0600

    cv added

---
+++ src/lib/ImageProvider.js
@@ -0,0 +1,28 @@
+
+import Resume1 from '@/assets/all_reumes/Resume1.png';
+import Resume2 from '@/assets/all_reumes/Resume2.png';
+import Resume3 from '@/assets/all_reumes/Resume3.png'
+import Resume4 from '@/assets/all_reumes/Resume4.png';
+import Resume5 from '@/assets/all_reumes/Resume5.png';
+import Resume6 from '@/assets/all_reumes/Resume6.png';
+import Resume7 from '@/assets/all_reumes/Resume7.png';
+import Resume8 from '@/assets/all_reumes/Resume8.png';
+import Resume9 from '@/assets/all_reumes/Resume9.png';
+import Resume10 from '@/assets/all_reumes/Resume10.png';
+import Resume11 from '@/assets/all_reumes/Resume11.png';
+
+
+
+export const ImageAssets = {
+    Resume1,
+    Resume2,
+    Resume3,
+    Resume4,
+    Resume5,
+    Resume6,
+    Resume7,
+    Resume8,
+    Resume9,
+    Resume10,
+    Resume11
+}
